dd400411fb4ab4f4d971654567452fbe4159378e
Pablo Oñate Villaverde <Pablo Oate Villaverde>
2017-06-10 11:00:37 +0200

    Is this working?

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>Welcome to this wonderful app</div>
+      <div>Is this working?</div>
     );
   }
 }
