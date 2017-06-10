0ed1a2e576187093f30f0a090aec640155a52409
Pablo Oñate Villaverde <Pablo Oate Villaverde>
2017-06-10 10:42:41 +0200

    Welcome message added

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter by Stephen Gride</div>
+      <div>Welcome to this wonderful app</div>
     );
   }
 }
